2e91966a8119ddf3334e563f49499368bd1daf9d
Jinacio23 <jinaciocemar@gmail.com>
2025-05-26 11:23:59 -0300

    adicionar lista a usuário

--- models/produto.js
+++ models/produto.js
@@ -6,15 +6,27 @@ module.exports = (sequelize, DataTypes) => {
       autoIncrement: true,
       allowNull: true
     },
-    nomeProduto: {type:DataTypes.STRING(45),allowNull: true},
-    descricao: {type:DataTypes.STRING(250),allowNull: true},
-    quantidade: {type:DataTypes.INTEGER,allowNull: true},
-    comprado: {type:DataTypes.BOOLEAN,allowNull: true},
-    categoria: {type:DataTypes.STRING(50),allowNull: true}
+    nomeProduto: { type: DataTypes.STRING(45), allowNull: true },
+    descricao: { type: DataTypes.STRING(250), allowNull: true },
+    quantidade: { type: DataTypes.INTEGER, allowNull: true },
+    comprado: { type: DataTypes.BOOLEAN, allowNull: true },
+    categoria: { type: DataTypes.STRING(50), allowNull: true }
   }, {
     tableName: 'tb_produto',
     timestamps: false
   });
+
+  Produto.associate = (models) => {
+    Produto.belongsToMany(models.ListaCompras, {
+      through: models.ListaProduto,
+      foreignKey: 'tb_Produto_id_Produto',
+      otherKey: 'tb_listaCompras_id'
+    });
+
+    Produto.hasMany(models.ListaProduto, {
+      foreignKey: 'tb_Produto_id_Produto'
+    });
+  };
 
   return Produto;
 };
