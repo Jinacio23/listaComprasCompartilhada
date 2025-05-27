e58fe00724104dd83989af47c98f519425f6b52a
José Inacio Fernandes da Silva <Jose52281256@edu.df.senac.br>
2025-05-27 11:20:51 -0300

    cadastro

--- src/view/cadastro/cadastro.js
+++ src/view/cadastro/cadastro.js
@@ -1,6 +1,6 @@
 const form = document.querySelector(".form");
 
-form.addEventListener('submit',async (event) => {
+form.addEventListener('submit', async (event) => {
   event.preventDefault();
 
   const nome = document.getElementById("name").value;
@@ -25,56 +25,17 @@ form.addEventListener('submit',async (event) => {
   } catch (err) {
     alert("erro ao cadastrar!");
   }
-
 })
 
 
-// function validarCadastro() {
-//     const email = document.getElementById('email').value;
-//     const senha = document.getElementById('password').value;
-  
+let currentSlide = 0;
+const slides = document.querySelectorAll('.carousel-slide');
 
-//     const requisitos = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/;
-  
-//     if (!email || !senha) {
-//       alert("Preencha todos os campos!");
-//       return;
-//     }
-  
-//     if (!requisitos.test(senha)) {
-//       alert("A senha não atende aos requisitos.");
-//       return;
-//     }
-  
-//     alert("Cadastro realizado com sucesso!");
-//   }
-    
-  let currentSlide = 0;
-  const slides = document.querySelectorAll('.carousel-slide');
-  const selectors = document.querySelectorAll('.selector');
-
-  function selectSlide(index) {
-    slides[currentSlide].classList.remove('active');
-    selectors[currentSlide].classList.remove('active');
-    currentSlide = index;
-    slides[currentSlide].classList.add('active');
-    selectors[currentSlide].classList.add('active');
-  }
-
-  function validarCadastro() {
-    const email = document.getElementById("email").value;
-    const senha = document.getElementById("password").value;
-
-    if (!email || !senha) {
-      alert("Por favor, preencha todos os campos.");
-      return;
-    }
-
-    if (senha.length < 8) {
-      alert("A senha deve conter no mínimo 8 caracteres.");
-      return;
-    }
-
-    alert("Cadastro realizado com sucesso!");
-  }
+function moveSlide(direction) {
+  slides[currentSlide].classList.remove('active');
+  currentSlide += direction;
+  if (currentSlide < 0) currentSlide = slides.length - 1;
+  if (currentSlide >= slides.length) currentSlide = 0;
+  slides[currentSlide].classList.add('active');
+}
 
