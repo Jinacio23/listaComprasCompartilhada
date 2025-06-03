31f531e7cf433de7aa0af1298abac7b3002d82bc
yuri <yuripolicantedealmeida@gmail.com>
2025-06-03 08:48:01 -0300

    colocando menu nas paginas

--- src/view/pagina principal/pagina principal.js
+++ src/view/pagina principal/pagina principal.js
@@ -1,3 +1,28 @@
-function redirecionarPara(pagina) {
-  window.location.href = pagina;
+
+// MENU LATERAL
+const openMenuBtn = document.getElementById("openMenuBtn");
+const closeMenuBtn = document.getElementById("closeMenuBtn");
+const sideMenu = document.getElementById("sideMenu");
+const sideMenuBg = document.getElementById("sideMenuBg");
+
+openMenuBtn.onclick = function() {
+  sideMenu.style.display = "flex";
+  setTimeout(() => sideMenuBg.classList.add("active"), 20);
+  openMenuBtn.classList.add("hidden"); // some o hamburguer
+};
+closeMenuBtn.onclick = function() {
+  sideMenu.style.display = "none";
+  sideMenuBg.classList.remove("active");
+  openMenuBtn.classList.remove("hidden"); // mostra o hamburguer
+};
+sideMenuBg.onclick = function(e) {
+  if (e.target === sideMenuBg) {
+    sideMenu.style.display = "none";
+    sideMenuBg.classList.remove("active");
+    openMenuBtn.classList.remove("hidden"); // mostra o hamburguer
+  }
+};
+
+function criarLista() {
+  // ação ao criar lista
 }
